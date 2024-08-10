027815e4e7fa3e8277604f806979fc5ff4e6190b
Abenezer <aben.ezer443@gmail.com>
2024-08-10 12:22:56 +0300

    deployment fixes

--- server/schemas.js
+++ server/schemas.js
@@ -1,21 +1,20 @@
 const BaseJoi = require("joi");
-const sanitizeHtml = require("sanitize-html");
 
 const extension = (joi) => ({
   type: "string",
   base: joi.string(),
   messages: {
-    "string.escapeHTML": "{{#label} must not include HTML!}",
+    "string.escapeHTML": "{{#label}} must not include HTML!",
   },
   rules: {
     escapeHTML: {
       validate(value, helpers) {
-        const clean = sanitizeHtml(value, {
-          allowedTags: [],
-          allowedAttributes: {},
-        });
-        if (clean != value)
+        const clean = value.replace(/<\/?[^>]+(>|$)/g, "");
+
+        if (clean !== value) {
           return helpers.error("string.escapeHTML", { value });
+        }
+
         return clean;
       },
     },
