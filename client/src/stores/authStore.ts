a55735d870271b90cf742458ebbd9a16b0c42d16
Abenezer <aben.ezer443@gmail.com>
2024-08-17 03:06:38 +0300

    fix login issues on production

--- client/src/stores/authStore.ts
+++ client/src/stores/authStore.ts
@@ -107,6 +107,7 @@ export const useAuthStore = create<AuthState>((set) => ({
     try {
       set({ loading: true });
       const res = await api.get("/getUser");
+      console.log(res);
       set({ user: res.data, loading: false, error: null });
     } catch {
       set({
