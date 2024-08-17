8d0aa0aba2492f60ac2b42fd62cf103d2e5b34ba
Abenezer <aben.ezer443@gmail.com>
2024-08-17 14:45:53 +0300

    fixed redirect issue on the backend

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", frontUrl);
+  res.header("Access-Control-Allow-Origin", [frontUrl]);
   res.header(
     "Access-Control-Allow-Methods",
     "GET, OPTIONS, PATCH, DELETE, POST, PUT"
